6649f80383d827d65d103e9d3e9525e417c74c91
Paola Kossa <paolakossa@gmail.com>
2022-09-01 11:46:55 -0300

    Constrution of the functions

--- main.js
+++ main.js
@@ -2,18 +2,48 @@ function trocaNome(){
 	let nome = document.querySelector('#nomesobrenome').value; 
 	document.querySelector('#card__front--nome').innerText = nome;
 
-}
+};
 
 function trocaNumero(){
 	let numero = document.querySelector('#numerodocartao').value; 
 	document.querySelector('#card__front--numero--num').innerText = numero;
+};
 
-}
-
-function trocaData() {
+function trocaDataMes() {
 	let validade = document.querySelector('#data').value;
 	document.querySelector('.card__front--validade-mes').innerText = validade;
 	
 	
-}
+};
 
+function trocaDataAno() {
+	let validade = document.querySelector('#data-ano').value;
+	document.querySelector('.card__front--validade-ano').innerText = validade;
+	
+	
+};
+
+function trocaCVC() {
+	let cvc = document.querySelector('#numerodeseguranca').value;
+	document.querySelector('.card__back--texto--p').innerText = cvc;
+	
+	
+};
+
+
+function enviar() {
+	document.querySelector('.mensagem__obrigado').style.display = 'block';
+
+	
+};
+
+const form = document.querySelector('.form')
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
+
+const btn = document.querySelector('.mensagem__obrigado-button');
+btn.addEventListener("click", function() {
+    
+    location.reload();
+});
