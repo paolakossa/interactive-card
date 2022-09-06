9b793cc04f6b1393f260019fbc3c1325a00f50bd
Paola Kossa <paolakossa@gmail.com>
2022-09-06 16:15:33 -0300

    Improve of function enviar

--- main.js
+++ main.js
@@ -1,41 +1,117 @@
 //Manipulação dos dados do cartão
 
 function trocaNome(){
-	let nome = document.querySelector('#nomesobrenome').value; 
+	const nome = document.querySelector('#nomesobrenome').value; 
 	document.querySelector('#card__front--nome').innerText = nome;
 
 };
 
 function trocaNumero(){
-	let numero = document.querySelector('#numerodocartao').value; 
+	const numero = document.querySelector('#numerodocartao').value; 
 	document.querySelector('#card__front--numero--num').innerText = numero;
 };
 
 function trocaDataMes() {
-	let validade = document.querySelector('#data').value;
+	const validade = document.querySelector('#data').value;
 	document.querySelector('.card__front--validade-mes').innerText = validade;
 	
 	
 };
 
 function trocaDataAno() {
-	let validade = document.querySelector('#data-ano').value;
+	const validade = document.querySelector('#data-ano').value;
 	document.querySelector('.card__front--validade-ano').innerText = validade;
 	
 	
 };
 
 function trocaCVC() {
-	let cvc = document.querySelector('#numerodeseguranca').value;
+	const cvc = document.querySelector('#numerodeseguranca').value;
 	document.querySelector('.card__back--texto--p').innerText = cvc;
 	
 	
 };
 
-// Mostrar a tela de obrigado 
+// Validar formulário e mostrar a tela de obrigado 
 
 function enviar() {
+	
+	const nome = document.querySelector('#nomesobrenome').value;
+	const numero = document.querySelector('#numerodocartao').value; 
+	const mes = document.querySelector('#data').value;
+	const ano = document.querySelector('#data-ano').value;
+	const cvc = document.querySelector('#numerodeseguranca').value;
+
+	if( nome === ''){
+
+	document.querySelector('.mensagem__obrigado').style.display = 'none';
+	document.querySelector('.mensagem__branco-1').style.display = 'block';
+	document.querySelector('#nomesobrenome').style.borderColor = 'hsl(0, 100%, 66%)';
+
+
+} else{
+
 	document.querySelector('.mensagem__obrigado').style.display = 'block';
+	document.querySelector('.mensagem__branco-1').style.display = 'none';
+	document.querySelector('#nomesobrenome').style.borderColor = 'hsl(270, 3%, 87%)';
+	
+}
+
+if( numero === ''){
+
+	document.querySelector('.mensagem__obrigado').style.display = 'none';
+	document.querySelector('.mensagem__erro').style.display = 'block';
+	document.querySelector('#numerodocartao').style.borderColor = 'hsl(0, 100%, 66%)';
+	
+} else{
+
+	document.querySelector('.mensagem__obrigado').style.display = 'block';
+	document.querySelector('.mensagem__erro').style.display = 'none';
+	document.querySelector('#numerodocartao').style.borderColor = 'hsl(270, 3%, 87%)';
+}
+
+if( mes === ''){
+
+	document.querySelector('.mensagem__obrigado').style.display = 'none';
+	document.querySelector('.mensagem__branco-2').style.display = 'block';
+	document.querySelector('#data').style.borderColor = 'hsl(0, 100%, 66%)';
+
+} else{
+
+	document.querySelector('.mensagem__obrigado').style.display = 'block';
+	document.querySelector('.mensagem__branco-2').style.display = 'none';
+	document.querySelector('#data').style.borderColor = 'hsl(270, 3%, 87%)';
+
+}
+
+if( ano === ''){
+
+	document.querySelector('.mensagem__obrigado').style.display = 'none';
+	document.querySelector('.mensagem__branco-3').style.display = 'block';
+	document.querySelector('#data-ano').style.borderColor = 'hsl(0, 100%, 66%)';
+
+} else{
+
+	document.querySelector('.mensagem__obrigado').style.display = 'block';
+	document.querySelector('.mensagem__branco-3').style.display = 'none';
+	document.querySelector('#data-ano').style.borderColor = 'hsl(270, 3%, 87%)';
+
+}
+
+if( cvc === ''){
+
+	document.querySelector('.mensagem__obrigado').style.display = 'none';
+	document.querySelector('.mensagem__branco-4').style.display = 'block';
+	document.querySelector('#numerodeseguranca').style.borderColor = 'hsl(0, 100%, 66%)';
+
+
+} else{
+
+	document.querySelector('.mensagem__obrigado').style.display = 'block';
+	document.querySelector('.mensagem__branco-4').style.display = 'none';
+	document.querySelector('#numerodeseguranca').style.borderColor = 'hsl(270, 3%, 87%)';
+}
+
 
 };
 
